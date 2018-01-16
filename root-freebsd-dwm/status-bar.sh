d48cb4f4ea6ceb3dbc1c0d3450f18d59acb33f25
Mateusz Piotrowski <mpp302@gmail.com>
2018-01-16 19:24:45 +0100

    Add xinitrc and a status bar script

---
+++ root-freebsd-dwm/status-bar.sh
@@ -0,0 +1,13 @@
+while true
+do
+    xsetroot -name "$(
+    acpiconf -i 0 | awk '\
+        /Remaining capacity*/{printf "%s", $3}\
+        /State:[[:space:]]+charging/{printf "+"}\
+        /Remaining time:[[:space:]]+.*:.*/{printf " (%s)", $3}'
+    printf -- ' | '
+    date +'%H:%m | %A, %d.%m'
+    )"
+
+    sleep 10
+done
