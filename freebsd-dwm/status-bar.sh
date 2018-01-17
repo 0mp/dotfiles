84c03b4abd75aa138b73dda0e998dbcbf6ef6c60
Mateusz Piotrowski <mpp302@gmail.com>
2018-01-17 01:13:32 +0100

    Move the looping to xinitrc from status-bar.sh
    This way it is possible to modify status-bar.sh on the fly.
    
    While here, rearrange the status bar.

--- freebsd-dwm/status-bar.sh
+++ freebsd-dwm/status-bar.sh
@@ -1,13 +1,8 @@
-while true
-do
-    xsetroot -name "$(
-    acpiconf -i 0 | awk '\
-        /Remaining capacity*/{printf "%s", $3}\
-        /State:[[:space:]]+charging/{printf "+"}\
-        /Remaining time:[[:space:]]+.*:.*/{printf " (%s)", $3}'
-    printf -- ' | '
-    date +'%H:%M | %A, %d.%m'
-    )"
-
-    sleep 10
-done
+xsetroot -name "$(
+acpiconf -i 0 | awk '\
+    /Remaining capacity*/{printf "%s", $3}\
+    /State:[[:space:]]+charging/{printf "+"}\
+    /Remaining time:[[:space:]]+.*:.*/{printf " (%s)", $3}'
+printf -- ' | '
+date +'%A, %d.%m | %H:%M'
+)"
