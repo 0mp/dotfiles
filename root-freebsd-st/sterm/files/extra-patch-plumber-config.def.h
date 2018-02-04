abfd0f0d115eae7c78bce665e051ea357bfe7ddf
Mateusz Piotrowski <mpp302@gmail.com>
2018-02-05 00:52:37 +0100

    Add plumber patches

---
+++ root-freebsd-st/sterm/files/extra-patch-plumber-config.def.h
@@ -0,0 +1,14 @@
+--- config.def.h.orig	2017-12-26 21:54:44 UTC
++++ config.def.h
+@@ -1,6 +1,11 @@
+ /* See LICENSE file for copyright .nd license details. */.orig
+
+ /*
++ * plum.er
++ */
++static char plumber[] = "~/.suckless/plumb.sh";
++
++/*
+  * appearance
+  *
+  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
