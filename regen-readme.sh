633e57222f15d50faa934324b4e49b55f531cdd5
Mateusz Piotrowski <0mp@FreeBSD.org>
2018-06-09 16:11:06 +0200

    Try to find mandoc supporting -Tmarkdown
    This is a relatively new feature and it is not available on older
    platforms like FreeBSD 11.1. Even when installed from ports it is still
    later in the PATH than the mandoc included in the base system.

--- regen-readme.sh
+++ regen-readme.sh
@@ -4,5 +4,15 @@
 
 set -e
 
-mandoc -Tmarkdown ./dotfiles.1 | awk '/# AUTHORS/{exit 0} NR > 2{print $0} ' > README.md.tmp
-mv README.md.tmp README.md
+for mandoc in $(which -a mandoc)
+do
+    if "$mandoc" -Tmarkdown ./dotfiles.1 >/dev/null 2>&1
+    then
+        "$mandoc" -Tmarkdown ./dotfiles.1 | \
+            awk '/# AUTHORS/{exit 0} NR > 2{print $0} ' > README.md.tmp
+        mv README.md.tmp README.md
+        exit 0
+    fi
+done
+echo 'None of the available mandoc support "-Tmarkdown"' >&2
+exit 1
