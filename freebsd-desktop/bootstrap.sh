b6740c35d897492aff22fb2c0f1ea7b3761d1272
Mateusz Piotrowski <0mp@FreeBSD.org>
2018-06-24 13:19:51 +0200

    Improve the bootstrap script for FreeBSD machines
     * Switch to the latest branch before installing any packages.
     * Bootstrap pkg(8) before using it for the first time.
     * Fix the su(1) usage.

--- freebsd-desktop/bootstrap.sh
+++ freebsd-desktop/bootstrap.sh
@@ -2,34 +2,6 @@ set -eux
 
 user="$1"
 
-# Install packages.
-packages='
-bash
-bash-completion
-firefox
-git
-sudo
-tmux
-vim
-xclip
-xorg
-'
-pkg install -y -- $packages
-
-# Download dotfiles.
-dotfilesurl='https://github.com/0mp/dotfiles'
-dotfilesdir="/home/$user/.dotfiles"
-if ! [ -d "$dotfilesdir" ]; then
-    su "$user" -c git clone "$dotfilesurl" "$dotfilesdir"
-fi
-
-# Enable sudo for the wheel group.
-printf '%s\n' 'g/# %wheel ALL=(ALL) ALL/s//%wheel ALL=(ALL) ALL/' 'w' | \
-    EDITOR='ed -s' visudo
-
-# Change the login shell to Bash.
-chsh -s bash "$user"
-
 # Configure pkg to use the "latest" branch instead of the "quaterly"
 # one.
 pkgconfdir='/usr/local/etc/pkg/repos'
@@ -47,3 +19,32 @@ FreeBSD: {
 }
 EOF
 fi
+
+# Install packages.
+packages='
+bash
+bash-completion
+firefox
+git
+sudo
+tmux
+vim
+xclip
+xorg
+'
+pkg bootstrap
+pkg install -y -- $packages
+
+# Download dotfiles.
+dotfilesurl='https://github.com/0mp/dotfiles'
+dotfilesdir="/home/$user/.dotfiles"
+if ! [ -d "$dotfilesdir" ]; then
+    su "$user" -c "git clone '$dotfilesurl' '$dotfilesdir'"
+fi
+
+# Enable sudo for the wheel group.
+printf '%s\n' 'g/# %wheel ALL=(ALL) ALL/s//%wheel ALL=(ALL) ALL/' 'w' | \
+    EDITOR='ed -s' visudo
+
+# Change the login shell to Bash.
+chsh -s bash "$user"
