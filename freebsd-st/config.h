a4f8b803b8df662c233004e9a502bce3dc2d8ab0
Mateusz Piotrowski <0mp@FreeBSD.org>
2018-07-15 02:43:16 +0200

    Increase the font to 14pt

--- freebsd-st/config.h
+++ freebsd-st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Inconsolata:pixelsize=14:antialias=true:autohint=true";
+static char *font = "Inconsolata:pixelsize=12:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
