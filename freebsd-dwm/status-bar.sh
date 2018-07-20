afc1c606bde197c569012a0b196b9ff4b11ee341
Mateusz Piotrowski <0mp@FreeBSD.org>
2018-07-20 17:43:14 +0200

    Remove unnecessary \'s from the end of lines

--- freebsd-dwm/status-bar.sh
+++ freebsd-dwm/status-bar.sh
@@ -8,9 +8,9 @@ mixer -S vol
 [ -f ~/.0mp-switch/eyes-alert-off ] && printf -- ' | !👓'
 printf -- ' | '
 [ -f ~/.0mp-switch/battery-alert-off ] && printf -- '! '
-acpiconf -i 0 | awk '\
-    /Remaining capacity*/{printf "%s", $3}\
-    /State:[[:space:]]+charging/{printf "+"}\
+acpiconf -i 0 | awk '
+    /Remaining capacity*/{printf "%s", $3}
+    /State:[[:space:]]+charging/{printf "+"}
     /Remaining time:[[:space:]]+.*:.*/{printf " (%s)", $3}'
 printf -- ' | '
 date +'%A, %d.%m | %H:%M'
