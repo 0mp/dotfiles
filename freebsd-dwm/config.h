86a1d2421fecc60a00ba6261048f54889996da07
Mateusz Piotrowski <0mp@FreeBSD.org>
2019-03-01 13:22:18 +0100

    Remove st2 support

--- freebsd-dwm/config.h
+++ freebsd-dwm/config.h
@@ -59,7 +59,6 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *term2cmd[]  = { "st2", NULL };
 static const char *volupcmd[]  = { "mixer", "vol", "+2", NULL };
 static const char *voldowncmd[]  = { "mixer", "vol", "-2", NULL };
 static const char *volmutecmd[]  = { "mixer", "vol", "0", NULL };
@@ -69,7 +68,6 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY|ControlMask|ShiftMask, XK_Return, spawn,          {.v = term2cmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
