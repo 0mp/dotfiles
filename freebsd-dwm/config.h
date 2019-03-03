c05870fbabe0c10913653cd3fddef065777798b8
Mateusz Piotrowski <0mp@FreeBSD.org>
2019-03-03 20:37:47 +0100

    Increase border width

--- freebsd-dwm/config.h
+++ freebsd-dwm/config.h
@@ -3,7 +3,7 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 4;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
