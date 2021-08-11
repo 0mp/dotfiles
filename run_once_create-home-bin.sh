5216c3b29465dd658ad68f682b2cea2c97377807
Mateusz Piotrowski <0mp@FreeBSD.org>
2021-08-11 17:25:12 +0200

    chezmoi: Preserve files in ~/bin when symlinking

--- run_once_create-home-bin.sh
+++ run_once_create-home-bin.sh
@@ -1,6 +1,20 @@
 #! /bin/sh -
 
-set -e
+set -eu
+
+tempdir=""
+
+if [ -d ~/bin ]; then
+	tempdir=$(mktemp -d)
+	test -d "$tempdir"
+	mv ~/bin "$tempdir/"
+fi
 
 mkdir -p ~/.local/bin
 ln -F -s ~/.local/bin ~/bin
+
+if [ -d "$tempdir" ]; then
+	find "$tempdir/bin" -mindepth 1 -maxdepth 1 -exec mv {} ~/.local/bin/ \;
+	rmdir "$tempdir/bin"
+	rmdir "$tempdir"
+fi
