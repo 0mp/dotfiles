470eb6e11a15262d44415a40898dff47405192e3
Mateusz Piotrowski <0mp@FreeBSD.org>
2021-10-17 21:38:01 +0200

    Add samsung-horizontal-left.sh

---
+++ dot_screenlayout/private_executable_samsung-horizontal-left.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --primary --mode 2560x1440 --pos 1680x0 --rotate normal --output DP-1 --off --output DP-2 --off --output HDMI-1 --off --output HDMI-2 --mode 1680x1050 --pos 0x195 --rotate normal --output VIRTUAL1 --off
