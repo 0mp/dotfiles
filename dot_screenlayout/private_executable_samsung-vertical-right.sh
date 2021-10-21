97d18fab9fc3a217d5ed36a968a783b8894ddd9e
Mateusz Piotrowski <0mp@FreeBSD.org>
2021-10-21 21:10:38 +0200

    Add two more scripts for screen layout configuration

---
+++ dot_screenlayout/private_executable_samsung-vertical-right.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --off --output HDMI-1 --off --output HDMI-2 --mode 1680x1050 --pos 2560x0 --rotate right --output VIRTUAL1 --off
